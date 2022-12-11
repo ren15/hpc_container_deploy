ac9aa5737ec7bf18e74322c4184cfced417ab58e
Ren Wang <iamwrm@gmail.com>
2022-12-11 09:34:09 +0000

    add deploy notes

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 
 - Test if singularity binary works in intel devcloud
 
+https://docs.sylabs.io/guides/3.5/admin-guide/configfiles.html#external-tooling-paths
--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,7 @@
 singularity config global --set "mksquashfs path" /home/u85490/workspace/sing_deploy/bin/
 
 
+singularity build --sandbox sin_root/ docker://rust:1.65.0
 
+singularity shell --writable sin_root/
+singularity run --writable sin_root/ cat /etc/os-release
