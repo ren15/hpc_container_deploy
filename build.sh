daabbf7a94ceec256405c79574014b1760669c09
Ren Wang <iamwrm@gmail.com>
2022-12-11 11:22:47 +0000

    should work

--- build.sh
+++ build.sh
@@ -2,8 +2,13 @@ docker build -t sing .
 
 mkdir -p sing_deploy
 rm -rf sing_deploy/* 
+    
+export HOST_UID=$(id -u)
+export HOST_GID=$(id -g)
 
-docker run --rm -u $(id -u):$(id -g) -v $(pwd)/sing_deploy:/app sing \
+docker run --rm \
+    -v $(pwd)/sing_deploy:/app \
+    sing \
     bash -c "git clone https://github.com/hpcng/singularity.git && \
     cd singularity && \
     git checkout v3.8.4 && \
@@ -12,7 +17,7 @@ docker run --rm -u $(id -u):$(id -g) -v $(pwd)/sing_deploy:/app sing \
     cd ./builddir && \
     make && \
     make install && \
-    "
+    chown -R $HOST_UID:$HOST_GID /app"
 
 wget -c http://launchpadlibrarian.net/439651704/squashfs-tools_4.4-1_amd64.deb
 
