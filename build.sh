6d7b582988bef432110b137f2e24afaa8fb58788
Ren Wang <iamwrm@gmail.com>
2022-12-11 11:00:38 +0000

    try fixing permision

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ docker build -t sing .
 mkdir -p sing_deploy
 rm -rf sing_deploy/* 
 
-docker run --rm -v $(pwd)/sing_deploy:/app sing \
+docker run --rm -u $(id -u):$(id -g) -v $(pwd)/sing_deploy:/app sing \
     bash -c "git clone https://github.com/hpcng/singularity.git && \
     cd singularity && \
     git checkout v3.8.4 && \
@@ -12,7 +12,6 @@ docker run --rm -v $(pwd)/sing_deploy:/app sing \
     cd ./builddir && \
     make && \
     make install && \
-    chown -R 1000:1000 /app
     "
 
 wget -c http://launchpadlibrarian.net/439651704/squashfs-tools_4.4-1_amd64.deb
